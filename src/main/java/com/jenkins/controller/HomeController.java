b440f0bb77aabeb7f7410f49e1f85593826212b0
unknown <nthai.uneti@gmail.com>
2021-03-11 16:31:28 +0700

    innit git

---
+++ src/main/java/com/jenkins/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.jenkins.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/")
+public class HomeController {
+
+    @GetMapping(value = "well-come/{user-name}")
+    public ResponseEntity<String> wellCome(
+            @PathVariable(value = "user-name") String userName) {
+        return ResponseEntity.ok("Well Come " + userName);
+    }
+
+}
